f4ee8fa5e711d63948644098918f15c88c93da01
Rajan <rajan.chauhan18@imperial.ac.uk>
2020-11-24 09:27:05 +0000

    ImageScraper methods broken down a bit. get_image_urls method made a bit more robust.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from src.scraper.image_scraper import ImageScraper
 
 if __name__ == '__main__':
     scraper = ImageScraper()
-    urls = scraper.fetch_image_urls('Dogs', 1, 2)
+    urls = scraper.get_image_urls('Dogs', 10, 1)
     for url in urls:
         scraper.persist_image('/Users/rajanchauhan/Desktop/Programming-Projects.nosync/Python '
                           'Projects/image-scraper/images', url)
--- src/scraper/image_scraper.py
+++ src/scraper/image_scraper.py
@@ -8,8 +8,7 @@ from selenium import webdriver
 from PIL import Image
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(process)d --- %(name)s %(funcName)20s() : %(message)s',
-                    datefmt='%d-%b-%y %H:%M:%S',
-                    level=logging.INFO)
+                    datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
 
 class ImageScraper:
@@ -18,55 +17,32 @@ class ImageScraper:
     def __init__(self):
         self.driver = webdriver.Chrome()
 
-    def fetch_image_urls(self, query: str, max_links_to_fetch: int, sleep_between_interactions: int = 1):
-        def scroll_to_end():
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(sleep_between_interactions)
-
-            # build the google query
-
+    def get_image_urls(self, query: str, max_urls: int, sleep_between_interactions: int = 1):
         search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
-
-        # load the page
         self.driver.get(search_url.format(q=query))
 
         image_urls = set()
         image_count = 0
         results_start = 0
-        while image_count < max_links_to_fetch:
-            scroll_to_end()
-
-            # get all image thumbnail results
+        while image_count < max_urls:
+            self.__scroll_to_end(sleep_between_interactions)
             thumbnail_results = self.driver.find_elements_by_css_selector("img.Q4LuWd")
             number_results = len(thumbnail_results)
-
             self.logger.info(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
 
             for img in thumbnail_results[results_start:number_results]:
-                # try to click every thumbnail such that we can get the real image behind it
-                try:
-                    img.click()
-                    time.sleep(sleep_between_interactions)
-                except Exception:
-                    continue
-
-                # extract image urls
-                actual_images = self.driver.find_elements_by_css_selector('img.n3VNCb')
-                for actual_image in actual_images:
-                    if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
-                        image_urls.add(actual_image.get_attribute('src'))
-
+                self.__click_and_wait(img, sleep_between_interactions)
+                self.__add_image_urls_to_set(image_urls)
                 image_count = len(image_urls)
-
-                if len(image_urls) >= max_links_to_fetch:
+                if image_count >= max_urls:
                     self.logger.info(f"Found: {len(image_urls)} image links, done!")
                     break
             else:
-                self.logger.info("Found:", len(image_urls), "image links, looking for more ...")
-                time.sleep(30)
-                return
+                self.logger.info(f"Found: {len(image_urls)} image links, looking for more ...")
+
                 load_more_button = self.driver.find_element_by_css_selector(".mye4qd")
                 if load_more_button:
+                    self.logger.info("loading more...")
                     self.driver.execute_script("document.querySelector('.mye4qd').click();")
 
             # move the result startpoint further down
@@ -75,7 +51,6 @@ class ImageScraper:
         return image_urls
 
     def persist_image(self, folder_path: str, url: str):
-
         image_content = self.__download_image_content(url)
         try:
             image_file = io.BytesIO(image_content)
@@ -87,8 +62,28 @@ class ImageScraper:
         except Exception as e:
             self.logger.error(f"ERROR - Could not save {url} - {e}")
 
+    def close_connection(self):
+        self.driver.quit()
+
     def __download_image_content(self, url):
         try:
             return requests.get(url).content
         except Exception as e:
             self.logger.error(f"ERROR - Could not download {url} - {e}")
+
+    def __scroll_to_end(self, sleep_time):
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(sleep_time)
+
+    def __click_and_wait(self, img, wait_time):
+        try:
+            img.click()
+            time.sleep(wait_time)
+        except Exception:
+            return
+
+    def __add_image_urls_to_set(self, image_urls: set):
+        actual_images = self.driver.find_elements_by_css_selector('img.n3VNCb')
+        for actual_image in actual_images:
+            if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
+                image_urls.add(actual_image.get_attribute('src'))
